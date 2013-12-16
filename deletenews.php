1d1dffc5df87b75559647564c8e46483bf83dab9
alisadikin008 <alisadikin008@gmail.com>
2013-12-16 11:03:22 +0700

    Revert "adding mysql_real_escape_string function"
    This reverts commit d59cd797cfcd40f4bbfc9e47fa50942d4accdfd3.

--- deletenews.php
+++ deletenews.php
@@ -1,6 +1,6 @@
 <?php
 include_once "config.php";
-$id=mysql_real_escape_string($_GET['id']);
+$id=$_GET['id'];
 $sql="DELETE FROM berita WHERE id=$id";
 $result=mysql_query($sql);
 echo"<meta http-equiv='refresh' content='0, url=index.php'>";
--- editnews.php
+++ editnews.php
@@ -1,6 +1,6 @@
 <?php
 include_once "config.php";
-$id=mysql_real_escape_string($_GET['id']);
+$id=$_GET['id'];
 $sql="SELECT * FROM berita WHERE id=$id";
 $result=mysql_query($sql);
 $row=mysql_fetch_array($result);
@@ -8,8 +8,8 @@ include_once "view/editberita_view.php";
 /*----start updating from here--------*/
 if(isset($_POST['update']))
 {
-$title=mysql_real_escape_string($_POST['title']);
-$content=mysql_real_escape_string($_POST['content']);
+$title=htmlentities($_POST['title']);
+$content=$_POST['content'];
 $sql="UPDATE berita SET title='$title',content='$content' WHERE id=$id";
 $result=mysql_query($sql);
 echo"<meta http-equiv='refresh' content='0, url=index.php'>";
